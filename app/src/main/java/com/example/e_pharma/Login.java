1aa12d33779847a57f43dea50b9cac2fe4ca30d5
SanjidaSheikh0135 <sanjidasheikh0135@gmail.com>
2021-09-28 01:14:06 +0100

    second comment

--- app/src/main/java/com/example/e_pharma/Login.java
+++ app/src/main/java/com/example/e_pharma/Login.java
@@ -24,6 +24,7 @@ public class Login extends AppCompatActivity {
 
 TextView Username,logPassword;
 //comment
+    //aliBroti
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
